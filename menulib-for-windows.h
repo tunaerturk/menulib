07f16e4157f468d6096b6598935b492ffaa385d1
Tuna Ertürk <73068443+tunaerturk@users.noreply.github.com>
2022-12-04 17:50:09 +0300

    Create menulib-for-windows.h

---
+++ menulib-for-windows.h
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Menu {
+private:
+	int process;
+public:
+	int operation;
+
+	void mmenu(string appname, string menu1, string menu2) {
+		cout << "Welcome to the " + appname << endl;
+		cout << "1 - " + menu1 << endl;
+		cout << "2 - " + menu2 << endl;
+		cout << "Select a operation: ";
+		cin >> process;
+		switch (process)
+		{
+		case 1:
+			system("cls");
+			operation = 1;
+			break;
+		case 2:
+			system("cls");
+			operation = 2;
+			break;
+		default:
+			cout << "Wrong opreation";
+			break;
+		}
+	}
+};
